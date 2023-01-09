93489858d0793fd825a04be31f602fedaf4590b7
ta96ra <95111900+ta96ra@users.noreply.github.com>
2023-01-09 02:28:02 +0000

    [update]Add devise,webpacker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # config.hosts << "6527b81035534b648cdfadd8cd365bc7.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+ 
+  #会員用(新規登録、ログイン)
+  # URL /customers/sign_in ...
+  devise_for :users, skip:[:passwords],controllers:{
+    registrations: "public/registrations",
+    sessions: 'public/sessions'  
+  }
+  #*skip:[:不要なルーティング]
+  
+  
+  #管理者用(ログイン)
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations,:passwords],controllers:{
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
