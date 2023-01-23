94b2a2192f9164654f6841db82b4521fad542384
ta96ra <95111900+ta96ra@users.noreply.github.com>
2023-01-23 15:26:51 +0000

    [update]Top-page-half

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    # @park = Park.find(params[:id])
     @parks = Park.where(status:true).all
+    @sorts = Park.where(status:true).all.order(average_evaluation: "DESC")
   end
 end
